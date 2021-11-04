0617043e2d68042e8ceb13840b972cad5dd757f0
by-robert <robertslavikcomcast@gmail.com>
2021-11-04 14:55:02 -0600

    empty project

--- index.ts
+++ index.ts
@@ -1,25 +1,3 @@
-// hash map of values with string names
-const numberMap = new Map([
-  [1, 'one'],
-  [2, 'two'],
-  [3, 'three'],
-  [4, 'four'],
-  [5, 'five'],
-  [6, 'six'],
-  [7, 'seven'],
-  [8, 'eight'],
-  [9, 'nine'],
-  [10, 'ten'],
-  [11, 'eleven'],
-  [12, 'twelve'],
-  [13, 'thirteen'],
-  [14, 'fourteen'],
-  [15, 'fifteen'],
-  [16, 'sixteen'],
-  [17, 'seventeen'],
-  [18, 'eighteen'],
-  [19, 'nineteen'],
-]);
 
 /**
  * funtion to take a number and return the string name of the number
@@ -27,53 +5,5 @@ const numberMap = new Map([
  * @returns string "one"
  */
 export function getNumber(input: number) {
-  if (input <= 0 || input >= 100) {
-    return 'unsupported value: ' + input;
-  }
-  if (input >= 20) {
-    return checkIfSecondDigitZero(input);
-  } else {
-    // less than 20, do switch
-    return numberMap.get(input);
-  }
-}
-
-function tensNumbers(input: number) {
-  switch (input) {
-    case 1:
-      return 'ten';
-    case 2:
-      return 'twenty';
-    case 3:
-      return 'thirty';
-    case 4:
-      return 'fourty';
-    case 5:
-      return 'fifty';
-    case 6:
-      return 'sixty';
-    case 7:
-      return 'seventy';
-    case 8:
-      return 'eighty';
-    case 9:
-      return 'ninety';
-    default:
-      return 'error';
-  }
-}
-
-function checkIfSecondDigitZero(input: number) {
-  const digits = input.toString();
-  if (digits.length === 2) {
-    const start = tensNumbers(Number(digits[0]));
-    const end = numberMap.get(Number(digits[1]));
-    if (end) {
-      return start + '-' + end;
-    } else {
-      return start;
-    }
-  } else {
-    return 'unsupported value: ' + input;
-  }
+  return '0'
 }
--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@ import { getNumber } from './index';
 import t from 'tap';
 
 t.test('numbers', (t) => {
-  t.equal(getNumber(0), 'unsupported value: 0', 'unsupported value: 0');
-  t.equal(getNumber(1), 'one', 'one');
+  t.equal(getNumber(0), '0', '0');
+
   t.end();
 });
